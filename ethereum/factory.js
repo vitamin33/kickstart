706ee8169b96758971620a62b440ab73c6f585ce
Vitalii Serbyn <vivitseser@gmail.com>
2022-06-10 13:18:56 +0300

    Update smart contract factory address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ const compiledFactory = require('../ethereum/build/CampaignFactory.json').Campai
 const stringAbi = JSON.stringify(compiledFactory.abi);
 const instance = new web3.eth.Contract(
     JSON.parse(stringAbi),
-    '0x3800740F6D3385cce618cC7C879A279E5D636460'
+    '0x2411818E933DC56421c80F044f8f1EF5E3F75682'
 );
 
 export default instance;
