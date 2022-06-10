09d20e27f88a98845ca4805d52363e0ebffa45cf
Vitalii Serbyn <vivitseser@gmail.com>
2022-06-10 21:53:06 +0300

    Create reqeust new.js

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class RequestNew extends Component {
+    render() {
+        return (
+            <h3>Create request</h3>
+        );
+    }
+}
+
+export default RequestNew;
